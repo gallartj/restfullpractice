dbdb6b9b6ae3a8be46a42b80e006286a6753a018
juanpablo.gallart <juanpablo.gallart@globant.com>
2023-09-27 15:11:19 -0300

    Avances en User

--- src/main/java/com/rest/webservices/restfullwebservices/User/User.java
+++ src/main/java/com/rest/webservices/restfullwebservices/User/User.java
@@ -1,15 +1,21 @@
 package com.rest.webservices.restfullwebservices.User;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private Integer id;
+    @Size(min = 2,message = "name size could'nt be less than 2.")
     private String name;
+    @Past
     private LocalDate birtDate;
 }
--- src/main/java/com/rest/webservices/restfullwebservices/User/UserResource.java
+++ src/main/java/com/rest/webservices/restfullwebservices/User/UserResource.java
@@ -1,11 +1,10 @@
 package com.rest.webservices.restfullwebservices.User;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -29,7 +28,7 @@ public class UserResource {
         return user;
     }
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -39,5 +38,9 @@ public class UserResource {
         return ResponseEntity
                 .created(location)
                 .build();
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        service.deleteById(id);
     }
 }
