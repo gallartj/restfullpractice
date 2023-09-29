9087f095d7ccc542c79848e16cc970b95034f14c
juanpablo.gallart <juanpablo.gallart@globant.com>
2023-09-29 15:18:11 -0300

    Post Entity. Pom xml

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,14 @@ values (current_date(),10002,'ravi');
 insert into users_details (birt_date,id,name)
 values (current_date(),10003,'sathish');
 
+insert into post(id,description,user_id)
+values(200001,'I want to learn AWS',10001);
+
+insert into post(id,description,user_id)
+values(200002,'I want to learn DevOps',10001);
+
+insert into post(id,description,user_id)
+values(200003,'I want to learn AWS Certified',10002);
+
+insert into post(id,description,user_id)
+values(200004,'I want to learn Multi Cloud',10002);
